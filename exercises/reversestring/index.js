b044c7e118b83b0693b3bdd0173ec67f3ba955cc
Ronald Almeida <ronald.almeida@reisystems.com>
2020-05-07 15:44:42 -0400

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) 
+{
+    return str.split('').reduce((rev,char) => char+rev,'');
+}
 
 module.exports = reverse;
+
+
+// function reverse(str) 
+// {
+//     const arr = str.split('');
+//     let revStr = '';
+//     for (const char of arr) {
+//         revStr = char + revStr;
+//     }
+//     return revStr;
+// }
+
+// function reverse(str) 
+// {
+//     return str.split('').reverse().join('');
+// }
