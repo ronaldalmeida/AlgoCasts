84e0c596f39121b678246b72635eeb3d059c5b09
Ronald Almeida <ronald.almeida@reisystems.com>
2020-05-11 14:28:02 -0400

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) 
+{
+    return Math.sign(n) * n.toString().replace('-','').split('').reverse().join('');
+}
 
 module.exports = reverseInt;
+
+
+// function reverseInt(n) 
+// {
+//     let revInt = + n.toString().replace('-','').split('').reverse().join('');
+//     return n >= 0 ? revInt : revInt * -1
+// }
